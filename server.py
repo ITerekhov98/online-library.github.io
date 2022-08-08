94023d44f542e7158c2c9fdbac1da90161bcefd8
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-08-08 16:28:20 +0300

    Split books list by pages

--- server.py
+++ server.py
@@ -1,25 +1,27 @@
 import json
-
+import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
 
 def on_reload():
+    os.makedirs('pages', exist_ok=True)
+    with open('books_description.json', 'r') as f:
+        books_description = json.loads(f.read())
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
     template = env.get_template('template.html')
-    with open('books_description.json', 'r') as f:
-        books_description = json.loads(f.read())
-        books_pairs = chunked(books_description, 2)
-    
-    rendered_page = template.render(
-        books_pairs=books_pairs
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for index, books_batch in enumerate(chunked(books_description, 20), start=1):
+        rendered_page = template.render(
+            books_pairs=chunked(books_batch, 2)
+        )
+        current_page = f'index{index}.html'
+        with open(f'pages/{current_page}', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
     print("Site rebuilt")
 
 on_reload()
